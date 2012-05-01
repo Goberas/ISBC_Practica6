07fd22062932f503bb657675530ddd1fb5ad5dec
Gobo Hormigo <gobocop69@gmail.com>
2012-05-01 18:54:45 +0200

    Tutorial eCo, añadido comportamiento de portero

--- src/t111209/entrenadores/Entrenador.java
+++ src/t111209/entrenadores/Entrenador.java
@@ -1,7 +1,9 @@
 package t111209.entrenadores;
 
+import EDU.gatech.cc.is.util.Vec2;
 import t111209.behaviours.Blocker;
 import t111209.behaviours.GoToBall;
+import t111209.behaviours.PorteroBehaviour;
 import teams.ucmTeam.Behaviour;
 import teams.ucmTeam.RobotAPI;
 import teams.ucmTeam.TeamManager;
@@ -15,6 +17,21 @@ public class Entrenador extends TeamManager {
 	
 	@Override
 	public void onTakeStep() {
+		// Si la pelota está en campo contrario ==> GoToBall
+		RobotAPI robot0 = _players[0].getRobotAPI();
+		// Convertimos las coordenadas de la pelota (egocéntricas)
+		// en coordenadas del campo, para saber en qué campo está
+		Vec2 ballPosition = robot0.toFieldCoordinates(robot0.getBall());
+		if (ballPosition.x * robot0.getFieldSide() > 0) {
+			if (_players[0].getBehaviour() != _behaviours[0]) {
+				_players[0].setBehaviour(_behaviours[0]);
+			}
+		} else {
+			// E.o.c. ==> Portero
+			if (_players[0].getBehaviour() != _behaviours[2]) {
+				_players[0].setBehaviour(_behaviours[2]);
+			}
+		}
 		// Si el jugador esta en su campo --> GoToBall
 		RobotAPI robot = _players[2].getRobotAPI();
 		if (robot.getPosition().x * robot.getFieldSide()>=0)
@@ -22,9 +39,7 @@ public class Entrenador extends TeamManager {
 		else
 			// E.o.c. --> Blocker
 			_players[2].setBehaviour(_behaviours[1]);
-		
-		if (robot.getMyScore()> robot.getOpponentScore())
-			_players[2].setBehaviour(_behaviours[1]);
+
 	}
 	
 	@Override
@@ -34,7 +49,7 @@ public class Entrenador extends TeamManager {
 	
 	@Override
 	public Behaviour[] createBehaviours() {
-		return new Behaviour[] {new GoToBall(), new Blocker()};
+		return new Behaviour[] {new GoToBall(), new Blocker(), new PorteroBehaviour()};
 		
 	}
 }
