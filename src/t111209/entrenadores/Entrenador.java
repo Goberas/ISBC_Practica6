6c59f24a5821d9897934f80c02504c2d4b4caab9
Gobo Hormigo <gobocop69@gmail.com>
2012-05-01 20:52:59 +0200

    Nuevo rol buscador, funciona como le da la gana pero por lo menos tira

--- src/t111209/entrenadores/Entrenador.java
+++ src/t111209/entrenadores/Entrenador.java
@@ -2,6 +2,7 @@ package t111209.entrenadores;
 
 import EDU.gatech.cc.is.util.Vec2;
 import t111209.behaviours.Blocker;
+import t111209.behaviours.BuscadorBehaviour;
 import t111209.behaviours.GoToBall;
 import t111209.behaviours.PorteroBehaviour;
 import teams.ucmTeam.Behaviour;
@@ -17,30 +18,17 @@ public class Entrenador extends TeamManager {
 	
 	@Override
 	public void onTakeStep() {
-/*		// Si la pelota está en campo contrario ==> GoToBall
-		RobotAPI robot0 = _players[0].getRobotAPI();
-		// Convertimos las coordenadas de la pelota (egocéntricas)
-		// en coordenadas del campo, para saber en qué campo está
-		Vec2 ballPosition = robot0.toFieldCoordinates(robot0.getBall());
-		if (ballPosition.x * robot0.getFieldSide() > 0) {
-			if (_players[0].getBehaviour() != _behaviours[0]) {
-				_players[0].setBehaviour(_behaviours[0]);
-			}
-		} else {
-			// E.o.c. ==> Portero
-			if (_players[0].getBehaviour() != _behaviours[2]) {
-				_players[0].setBehaviour(_behaviours[2]);
-			}
-		}
-		*/
 		
 		if (_players[0].getBehaviour() != _behaviours[2]) 
 			_players[0].setBehaviour(_behaviours[2]);
+		if (_players[1].getBehaviour() != _behaviours[3]) 
+			_players[1].setBehaviour(_behaviours[3]);
+		
 		// Si el jugador esta en su campo y no va ganando --> GoToBall
 		RobotAPI robot = _players[2].getRobotAPI();
 		if ((robot.getPosition().x * robot.getFieldSide()>=0) &&
 			(robot.getMyScore() <= robot.getOpponentScore())){
-			for(int i=1; i<5;i++)
+			for(int i=2; i<5;i++)
 				_players[i].setBehaviour(_behaviours[0]);
 		}
 		else
@@ -56,6 +44,9 @@ public class Entrenador extends TeamManager {
 		case 0:
 			return _behaviours[2];
 			
+		case 1:
+			return _behaviours[3];
+			
 		default:
 			return _behaviours[0];
 		}
@@ -65,7 +56,10 @@ public class Entrenador extends TeamManager {
 	
 	@Override
 	public Behaviour[] createBehaviours() {
-		return new Behaviour[] {new GoToBall(), new Blocker(), new PorteroBehaviour()};
+		return new Behaviour[] {new GoToBall(), 
+								new Blocker(), 
+								new PorteroBehaviour(),
+								new BuscadorBehaviour()};
 		
 	}
 }
